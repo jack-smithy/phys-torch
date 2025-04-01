824c47e39243325e5b297688aa985729e3b0c173
Jack Smith <jacksmith2000@me.com>
2025-04-01 17:30:01 +0100

    packaged

---
+++ hello.py
@@ -0,0 +1,9 @@
+from phys_torch import testies
+
+
+def main():
+    print(f"Hello from phys-torch! {testies((2, 2))}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ phys_torch/__init__.py
@@ -0,0 +1,5 @@
+import torch
+
+
+def testies(shape):
+    return torch.rand(shape)
